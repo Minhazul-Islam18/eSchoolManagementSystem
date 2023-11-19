f713096b29ba4e18311bb901c734f883301f9cd2
Minhazul-Islam18 <minhaz019166@email.com>
2023-11-19 16:42:48 +0600

    dashboard bug fixed

--- app/Livewire/Backend/DashboardComponent.php
+++ app/Livewire/Backend/DashboardComponent.php
@@ -2,13 +2,20 @@
 
 namespace App\Livewire\Backend;
 
-use Livewire\Attributes\Layout;
-use Livewire\Attributes\Title;
+use App\Models\User;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
 
 class DashboardComponent extends Component
 {
+    use Authorizable;
     #[Title('Dashboard')]
+    public function mount(User $user)
+    {
+        $this->authorize('app.roles.index', $user);
+    }
     public function render()
     {
         return view('livewire.backend.dashboard-component');
