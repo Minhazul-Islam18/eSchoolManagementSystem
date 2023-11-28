1c66adea202a217170e177efae2c1d931e1f14b5
Minhazul-Islam18 <minhaz019166@gmail.com>
2023-11-28 15:36:09 +0600

    Fields added to trx table

---
+++ database/migrations/2023_11_28_093406_add_fields_to_bkash_transections_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bkash_transections', function (Blueprint $table) {
+            $table->after('payment_id', function (Blueprint $table) {
+                $table->string('trx_id')->nullable();
+                $table->string('customer_msisdn')->nullable();
+                $table->string('transaction_reference')->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bkash_transections', function (Blueprint $table) {
+            //
+        });
+    }
+};
