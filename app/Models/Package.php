5d42b57e9bce321d8b628f6fe8a55a4ccd032483
Minhazul-Islam18 <minhaz019166@gmail.com>
2023-11-28 07:46:41 +0600

    ...

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -9,4 +9,9 @@ class Package extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function getIsFreeAttribute()
+    {
+        return empty($this->attributes['price']);
+    }
 }
