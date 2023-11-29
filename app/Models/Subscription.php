776ca1e1482bb88a4ac68e2d9329a8bbb3f72bd5
Minhazul-Islam18 <minhaz019166@gmail.com>
2023-11-29 10:37:45 +0600

    subscription model prepared

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Subscription extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    /**
+     * Get the user that owns the Subscription
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the package that owns the Subscription
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function package(): BelongsTo
+    {
+        return $this->belongsTo(Package::class);
+    }
 }
