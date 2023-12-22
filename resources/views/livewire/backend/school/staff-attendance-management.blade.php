5a3088b08656fbd0c8491a0169e6c5b0daf26844
Minhazul-Islam18 <minhaz019166@gmail.com>
2023-12-22 23:22:44 +0600

    loader updated

--- resources/views/livewire/backend/school/staff-attendance-management.blade.php
+++ resources/views/livewire/backend/school/staff-attendance-management.blade.php
@@ -11,61 +11,6 @@
             <div>
                 <h4 class="text-2xl mt-4 mb-2">Search for attendance sheat</h4>
                 <form wire:submit='getAttendanceSheet' class="flex gap-4 justify-start items-end">
-                    {{-- <div class="">
-                        <label for="" class="form-label">Sta</label>
-                        <select wire:model.blur='class_id' class="form-select rounded" wire:change='getSection'
-                            id="">
-                            <option value="">Select class</option>
-                            @foreach ($this->classes as $item)
-                                <option value="{{ $item->id }}"
-                                    {{ $item->id == $this->class_id ? 'selected' : '' }}>
-                                    {{ $item->class_name }}
-                                </option>
-                            @endforeach
-                        </select>
-                        @error('class_id')
-                            <span class="text-sm text-red-500">{{ $message }}</span>
-                        @enderror
-                    </div> --}}
-
-                    {{--  @if ($this->groups != null)
-                        <div class="">
-                            <label for="group_id" class="form-label">Groups</label>
-                            <select wire:model.blur='group_id' class="form-select rounded"
-                                wire:loading.class='opacity-50 blur-sm' wire:target='getSection' id="group_id">
-                                <option value="">Select group</option>
-                                @forelse ($this->groups as $item)
-                                    <option value="{{ $item->id }}"
-                                        {{ $item->id == $this->group_id ? 'selected' : '' }}>
-                                        {{ $item->group_name }}</option>
-                                @empty
-                                    <option value="" disabled>No group found</option>
-                                @endforelse
-                            </select>
-                            @error('group_id')
-                                <span class="text-sm text-red-500">{{ $message }}</span>
-                            @enderror
-                        </div>
-                    @else
-                        <div class="">
-                            <label for="section_id" class="form-label">Section</label>
-                            <select wire:model.blur='section_id' class="form-select rounded"
-                                wire:loading.class='opacity-50 blur-sm' wire:target='getSection' id="section_id">
-                                <option value="">Select section</option>
-                                @forelse ($sections as $item)
-                                    <option value="{{ $item->id }}"
-                                        {{ $item->id == $this->section_id ? 'selected' : '' }}>
-                                        {{ $item->section_name }}</option>
-                                @empty
-                                    <option value="" disabled>No section found
-                                    </option>
-                                @endforelse
-                            </select>
-                            @error('section_id')
-                                <span class="text-sm text-red-500">{{ $message }}</span>
-                            @enderror
-                        </div>
-                    @endif --}}
                     <div class="">
                         <label for="" class="form-label">Date</label>
                         <input wire:model.blur='attendance_date' type="date" class="form-input rounded"
@@ -78,10 +23,27 @@
                         class="px-12 py-2 bg-emerald-500/90 hover:bg-emerald-500 rounded-full">Search</button>
                 </form>
             </div>
+            <div class="my-4 relative h-[250px] flex items-center justify-center transition-all duration-200 ease-in-out"
+                wire:loading wire:target='getAttendanceSheet'>
+                <div class="loader">
+                    <div class="cell d-0"></div>
+                    <div class="cell d-1"></div>
+                    <div class="cell d-2"></div>
 
-            <img wire:loading wire:target='getAttendanceSheet'
-                src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*TBQCEJX9IO3JAOLpuR0xjg.gif"
-                alt="">
+                    <div class="cell d-1"></div>
+                    <div class="cell d-2"></div>
+
+
+                    <div class="cell d-2"></div>
+                    <div class="cell d-3"></div>
+
+
+                    <div class="cell d-3"></div>
+                    <div class="cell d-4"></div>
+
+
+                </div>
+            </div>
 
             <div class=" border-dashed border-4 rounded-lg py-8 px-4 mt-4 border-orange-400">
                 @if ($attendanceSheat == true)
@@ -95,6 +57,98 @@
     </main>
 </div>
 @push('page-style')
+    <style>
+        .loader {
+            --cell-size: 52px;
+            --cell-spacing: 1px;
+            --cells: 3;
+            --total-size: calc(var(--cells) * (var(--cell-size) + 2 * var(--cell-spacing)));
+            display: flex;
+            flex-wrap: wrap;
+            width: var(--total-size);
+            height: var(--total-size);
+        }
+
+        .cell {
+            flex: 0 0 var(--cell-size);
+            margin: var(--cell-spacing);
+            background-color: transparent;
+            box-sizing: border-box;
+            border-radius: 4px;
+            animation: 1.5s ripple ease infinite;
+        }
+
+        .cell.d-1 {
+            animation-delay: 100ms;
+        }
+
+        .cell.d-2 {
+            animation-delay: 200ms;
+        }
+
+        .cell.d-3 {
+            animation-delay: 300ms;
+        }
+
+        .cell.d-4 {
+            animation-delay: 400ms;
+        }
+
+        .cell:nth-child(1) {
+            --cell-color: #00FF87;
+        }
+
+        .cell:nth-child(2) {
+            --cell-color: #0CFD95;
+        }
+
+        .cell:nth-child(3) {
+            --cell-color: #17FBA2;
+        }
+
+        .cell:nth-child(4) {
+            --cell-color: #23F9B2;
+        }
+
+        .cell:nth-child(5) {
+            --cell-color: #30F7C3;
+        }
+
+        .cell:nth-child(6) {
+            --cell-color: #3DF5D4;
+        }
+
+        .cell:nth-child(7) {
+            --cell-color: #45F4DE;
+        }
+
+        .cell:nth-child(8) {
+            --cell-color: #53F1F0;
+        }
+
+        .cell:nth-child(9) {
+            --cell-color: #60EFFF;
+        }
+
+        /*Animation*/
+        @keyframes ripple {
+            0% {
+                background-color: transparent;
+            }
+
+            30% {
+                background-color: var(--cell-color);
+            }
+
+            60% {
+                background-color: transparent;
+            }
+
+            100% {
+                background-color: transparent;
+            }
+        }
+    </style>
 @endpush
 @push('page-script')
     <script>
--- resources/views/livewire/backend/school/student-attendance-management.blade.php
+++ resources/views/livewire/backend/school/student-attendance-management.blade.php
@@ -79,9 +79,27 @@
                 </form>
             </div>
 
-            <img wire:loading wire:target='getAttendanceSheet'
-                src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*TBQCEJX9IO3JAOLpuR0xjg.gif"
-                alt="">
+            <div class="my-4 relative h-[250px] flex items-center justify-center transition-all duration-200 ease-in-out"
+                wire:loading wire:target='getAttendanceSheet'>
+                <div class="loader">
+                    <div class="cell d-0"></div>
+                    <div class="cell d-1"></div>
+                    <div class="cell d-2"></div>
+
+                    <div class="cell d-1"></div>
+                    <div class="cell d-2"></div>
+
+
+                    <div class="cell d-2"></div>
+                    <div class="cell d-3"></div>
+
+
+                    <div class="cell d-3"></div>
+                    <div class="cell d-4"></div>
+
+
+                </div>
+            </div>
             <div class=" border-dashed border-4 rounded-lg py-8 px-4 mt-4 border-orange-400">
                 @if ($attendanceSheat == true)
                     <livewire:attendance-sheat-table :data="$students" />
@@ -94,6 +112,98 @@
     </main>
 </div>
 @push('page-style')
+    <style>
+        .loader {
+            --cell-size: 52px;
+            --cell-spacing: 1px;
+            --cells: 3;
+            --total-size: calc(var(--cells) * (var(--cell-size) + 2 * var(--cell-spacing)));
+            display: flex;
+            flex-wrap: wrap;
+            width: var(--total-size);
+            height: var(--total-size);
+        }
+
+        .cell {
+            flex: 0 0 var(--cell-size);
+            margin: var(--cell-spacing);
+            background-color: transparent;
+            box-sizing: border-box;
+            border-radius: 4px;
+            animation: 1.5s ripple ease infinite;
+        }
+
+        .cell.d-1 {
+            animation-delay: 100ms;
+        }
+
+        .cell.d-2 {
+            animation-delay: 200ms;
+        }
+
+        .cell.d-3 {
+            animation-delay: 300ms;
+        }
+
+        .cell.d-4 {
+            animation-delay: 400ms;
+        }
+
+        .cell:nth-child(1) {
+            --cell-color: #00FF87;
+        }
+
+        .cell:nth-child(2) {
+            --cell-color: #0CFD95;
+        }
+
+        .cell:nth-child(3) {
+            --cell-color: #17FBA2;
+        }
+
+        .cell:nth-child(4) {
+            --cell-color: #23F9B2;
+        }
+
+        .cell:nth-child(5) {
+            --cell-color: #30F7C3;
+        }
+
+        .cell:nth-child(6) {
+            --cell-color: #3DF5D4;
+        }
+
+        .cell:nth-child(7) {
+            --cell-color: #45F4DE;
+        }
+
+        .cell:nth-child(8) {
+            --cell-color: #53F1F0;
+        }
+
+        .cell:nth-child(9) {
+            --cell-color: #60EFFF;
+        }
+
+        /*Animation*/
+        @keyframes ripple {
+            0% {
+                background-color: transparent;
+            }
+
+            30% {
+                background-color: var(--cell-color);
+            }
+
+            60% {
+                background-color: transparent;
+            }
+
+            100% {
+                background-color: transparent;
+            }
+        }
+    </style>
 @endpush
 @push('page-script')
     <script>
